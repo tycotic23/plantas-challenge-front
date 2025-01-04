20361c0b8c92e04e020c37720175321f5e0ffa53
tycotic23 <magui.maggie@gmail.com>
2025-01-04 20:29:19 -0300

    main dashboard design

---
+++ src/app/action-bar/action-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'action-bar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './action-bar.component.html',
+  styleUrl: './action-bar.component.css'
+})
+export class ActionBarComponent {
+
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ActionBarComponent } from '../action-bar/action-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ActionBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
