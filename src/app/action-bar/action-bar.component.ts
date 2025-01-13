fcf5451cbbfb5feda121bf4e1b98c5d6a631dc35
tycotic23 <magui.maggie@gmail.com>
2025-01-12 23:14:29 -0300

    responsive

--- src/app/action-bar/action-bar.component.ts
+++ src/app/action-bar/action-bar.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TokenService } from '../services/token.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'action-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './action-bar.component.html',
   styleUrl: './action-bar.component.css'
 })
 export class ActionBarComponent {
   constructor(private tokenService:TokenService){}
+
+  toggleMenu:boolean=false;
 
   logOut():void{
     this.tokenService.logOut();
